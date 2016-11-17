9468149474203c9fb8378f4c5598f6c0c6f3b510
Ben Zimmerman <benzim@umich.edu>
2016-11-17 10:14:14 -0500

    Now with user input

--- Riot-Games-API.py
+++ Riot-Games-API.py
@@ -2,6 +2,8 @@
 # Rate Limits:
 # 500 requests every 10 minutes
 # 10 requests every 10 seconds
+
+# more info: https://github.com/pseudonym117/Riot-Watcher
 from riotwatcher import RiotWatcher
 
 w = RiotWatcher("RGAPI-76f4bcb6-da64-455a-aad2-07eee7ad0e8f")
@@ -9,5 +11,9 @@ w = RiotWatcher("RGAPI-76f4bcb6-da64-455a-aad2-07eee7ad0e8f")
 #check to see if we have API Calls Remaining 
 print (w.can_make_request()) 
 
-me = w.get_summoner(name = 'Comply')
-print (me)
+who_are_you = str(input("Please enter your summoner name:"))
+summoner = w.get_summoner(name = who_are_you)
+print (summoner)
+
+my_ranked_stats = w.get_ranked_stats(summoner['id'])
+#print(my_ranked_stats)
