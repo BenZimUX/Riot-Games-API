e422211bd3efbea27c5657bc4a30aabd38b7ccc5
Ben Zimmerman <benzim@umich.edu>
2016-11-21 14:41:26 -0500

    Some more data points

--- Riot-Games-API.py
+++ Riot-Games-API.py
@@ -2,6 +2,7 @@
 # Rate Limits:
 # 500 requests every 10 minutes
 # 10 requests every 10 seconds
+# Regional Endpoint Platform ID for North America: NA1
 
 # more info: https://github.com/pseudonym117/Riot-Watcher
 from riotwatcher import RiotWatcher
@@ -14,6 +15,12 @@ print (w.can_make_request())
 who_are_you = str(input("Please enter your summoner name:"))
 summoner = w.get_summoner(name = who_are_you)
 print (summoner)
+summoner_id = summoner['id']
 
-my_ranked_stats = w.get_ranked_stats(summoner['id'])
+my_ranked_stats = w.get_ranked_stats(summoner_id)
 #print(my_ranked_stats)
+
+my_recent_games = w.get_recent_games(summoner_id)
+#print (my_recent_games)
+
+champion_list = w.static_get_champion_list()
